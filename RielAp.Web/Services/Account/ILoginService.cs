106adbd4e2e8e70d22b7bf91e6aaab2421848e06
Taras <taras.horyachko@gmail.com>
2015-03-31 20:40:47 +0300

    add login service

---
+++ RielAp.Web/Services/Account/ILoginService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RielAp.Web.Models;
+using System.Threading.Tasks;
+
+namespace RielAp.Web.Services.Account
+{
+    interface ILoginService
+    {
+        string Login(LoginViewModel model);
+    }
+}
---
+++ RielAp.Web/Services/Account/Implementation/FormLoginUserService.cs
@@ -0,0 +1,57 @@
+﻿using RielAp.Core;
+using RielAp.Domain.Models;
+using RielAp.Web.Infrastructure;
+using RielAp.Web.Infrastructure.Abstract;
+using RielAp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RielAp.Web.Services.Account.Implementation
+{
+    public class FormLoginUserService: ILoginService
+    {
+        private IAuthProvider authProvider;
+
+        public string Login(LoginViewModel model)
+        {
+            /*string phone = StringHelper.GetOnlyNumbers(model.Phone);
+            if (phone.Length == 12)
+            {
+                AuthResult authResult = _authProvider.Authenticate(phone, model.Password);
+                if (authResult == AuthResult.Success)
+                {
+                    User user = _userRepository.GetUserByPhone(phone);
+                    if (user != null)
+                    {
+                        if (user.ProfileExpires.HasValue)
+                        {
+                            if (user.ProfileExpires.Value.CompareTo(DateTime.Now) < 0)
+                            {
+                                user.Profile = _profilesRepository.GetBasicProfile();
+                                user.ProfileExpires = null;
+                            }
+                        }
+
+                        if (!string.IsNullOrEmpty(user.TemporaryPassword))
+                        {
+                            user.Password = user.TemporaryPassword;
+                            user.TemporaryPasswordExpires = null;
+                            user.TemporaryPassword = null;
+                        }
+
+                        _userRepository.SaveChanges();
+                        return RedirectToLocal(returnUrl);
+                    }
+                }
+                else if (authResult == AuthResult.NoRights)
+                {
+
+                    message = Translation.Translation.AccessIsDeniedMessage;
+                }
+            }*/
+            return Translation.Translation.AccessIsDeniedMessage;
+        }
+    }
+}
